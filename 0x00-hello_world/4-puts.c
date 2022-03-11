b296516223f6305acc1a621940f9aea341316e5f
yakobsolo <yakisolo2009@gmail.com>
2022-03-10 22:31:55 -0800

    c program ,Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * 
+ * Return : Always 0(Success)
+ */
+int main(void)
+{
+ puts("\" Programing is ike building a multililngual puzzle");
+ 
+return(0)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
