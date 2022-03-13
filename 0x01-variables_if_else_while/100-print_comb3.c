552d5d58ab6ebd9b3a12c7e9a710487479ac3ca6
yakobsolo <yakisolo2009@gmail.com>
2022-03-13 11:37:24 -0700

    program that prints all possible different combinations of two digits.

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 /**
  * main - entry point
- * Retrun: returns 0 (success)
+ * Return: returns 0 (success)
  */
 int main(void)
 {
 	int i;
 	int j;
 
-	for(i = 48; i <= 57 ; i++)
+	for (i = 48; i <= 57 ; i++)
 	{
-		for(j = 48; j <= 57; j++)
+		for (j = 48; j <= 57; j++)
 		{
-			if ((i == 49 && j == 48) || (i == 50 && j == 49) || (i == 51 && j == 50) || (i == 52 && j == 51) || (i == 53 && j == 52) || (i == 54 && j == 53) || (i == 55 && j == 54) || (i == 56 && j ==55) || (i == 57 && j == 56) )
+			if (i == 49 && j == 48)
 			{
 				continue;
 			}
