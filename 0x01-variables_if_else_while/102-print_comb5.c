179080f91058b3b6954a384633809b5797ce125d
yakobsolo <yakisolo2009@gmail.com>
2022-03-13 12:44:46 -0700

    program that prints all possible combinations of two two-digit numbers.

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: returns 0 success
+ */
+int main(void)
+{
+	int c;
+
+	for(c == 0; c <=8; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
