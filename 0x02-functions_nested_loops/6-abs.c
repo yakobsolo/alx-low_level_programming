a3d67cd1b7978c98f0e48734ac79bf39eeab2e0d
yakobsolo <yakisolo2009@gmail.com>
2022-03-15 12:41:57 -0700

    function that computes the absolute value of an integer.

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,9 +3,11 @@
 /**
  * _abs - function that compute the absolute value of an integer
  * @c: accepts an integer c
+ * Return: retruns res
  */
 int _abs(int c)
 {
 	int res = abs(c);
+
 	return (res);
 }
