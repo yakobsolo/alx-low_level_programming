49a9f5ff1eb80a5698ff42667e980c7463ab3139
yakobsolo <yakisolo2009@gmail.com>
2022-03-15 03:37:45 -0700

    my first readme.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "_putchar.c"
+/**
+ * main - entry point
+ * Return: retruns o success
+ */
+int _putchar(char c);
+
+int main(void)
+{
+	_putchar('_');
+	return (0);
+}
+	
+
