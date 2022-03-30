0d7cc033b477b32c3a1bf4611b92399a44e9a40c
yakobsolo <yakisolo2009@gmai.com>
2022-03-30 21:16:00 +0000

    function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0.

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,11 +8,11 @@
 
 int _strlen_recursion(char *s)
 {
-		if (!*s)
-				{
-							return (0);
-								}
-			return (1 + _strlen_recursion(++s));
+	if (!*s)
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(++s));
 }
 
 /**
@@ -24,16 +24,15 @@ int _strlen_recursion(char *s)
 
 int p1(char *s, int l)
 {
-		if (l < 1)
-				{
-							return (1);
-								}
-
-			if (*s == *(s + l))
-					{
-								return (p1(s + 1, l - 2));
-									}
-				return (0);
+	if (l < 1)
+	{
+		return (1);
+	}
+	if (*s == *(s + l))
+	{
+		return (p1(s + 1, l - 2));
+	}
+	return (0);
 }
 
 /**
@@ -44,7 +43,7 @@ int p1(char *s, int l)
 
 int is_palindrome(char *s)
 {
-		int len = _strlen_recursion(s);
+	int len = _strlen_recursion(s);
 
-			return (p1(s, len - 1));
+	return (p1(s, len - 1));
 }
