10b63077590a797678e9d7e90d23b975ee1914fc
yakobsolo <yakisolo2009@gmai.com>
2022-04-04 10:58:05 +0000

    unction that returns a pointer to a newly allocated space in memory,

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,19 +11,27 @@ char *_strdup(char *str)
 {
 	unsigned int i;
 	char *s = NULL;
+	int count;
 
-	s = (char *)malloc(sizeof(char) * strlen(str));
+	for (count = 0; str[count] != '\0'; count++)
+	{
+	}
+	s = (char *)malloc(sizeof(char) * count);
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	if (s != NULL)
+	else if (s != NULL)
 	{
 		for (i = 0; str[i] != '\0'; i++)
 		{
 			s[i] = str[i];
 		}
 		s[i] = '\0';
+	}
+	else
+	{
+		return (NULL);
 	}
 	return (s);
 }
