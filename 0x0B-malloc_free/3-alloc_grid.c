0a224c52203f4f4ec787703fbd194077a28c73a4
yakobsolo <yakisolo2009@gmai.com>
2022-04-05 21:00:03 +0000

    commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,12 +1,10 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-
 /**
- *  * alloc_grid - Entry point
- *   *@width: rows of matrix
- *    *@height: columns of string
- *     * Return: a pointer to a 2 dimensional array of integers or null
+ * alloc_grid - Entry point
+ * @width: rows of matrix
+ * @height: columns of string
+ * Return: a pointer to a 2 dimensional array of integers or null
  */
 int **alloc_grid(int width, int height)
 {
@@ -17,7 +15,9 @@ int **alloc_grid(int width, int height)
 	int *p;
 	
 	if (width <= 0 || height <= 0)
+	{
 		return (NULL);
+	}
 	matrix = (int **)malloc(height * sizeof(int *));
 	if (matrix == NULL)
 		return (NULL);
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,19 +1,20 @@
 #include "main.h"
+#include <stdlib.h>
 /**
- *  * free_grid - allocates a grid, make space and free space
- *   * @grid: takes in width of grid
- *    * @height: height of grid
- *     * Return: free grid
+ *  * free_grid - Entry point
+ *   *@grid: rows of matrix
+ *    *@height: columns of string
+ *     * Return: a pointer to a 2 dimensional array of integers or null
  */
-
 void free_grid(int **grid, int height)
 {
-		int i;
-		
-		for (i = 0; i < height; i++)
-					{
-								free(grid[i]);
-									}
+	int i;
+	int *p;
 
-				free(grid);
+	for (i = 0; i < height; i++)
+	{
+		p = grid[i];
+		free(p);
+	}
+	free(grid);
 }
