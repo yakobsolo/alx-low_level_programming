a2d6865ccd5b11c16b522f507d95c40493089b11
yakobsolo <yakisolo2009@gmai.com>
2022-04-05 20:38:36 +0000

    commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,43 +1,46 @@
 #include "main.h"
-/**
- *  * alloc_grid - allocates a grid, make space and free space
- *   * @width: takes in width of grid
- *    * @height: height of grid
- *     * Return: grid with freed spaces
- */
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+ *  * alloc_grid - Entry point
+ *   *@width: rows of matrix
+ *    *@height: columns of string
+ *     * Return: a pointer to a 2 dimensional array of integers or null
+ *      */
 int **alloc_grid(int width, int height)
 {
-	/*Declaring variables*/
-	int **grid;
-	int i, j;
+		int **matrix;
+			int i;
+				int j;
+					int l;
+						int *p;
 
-	if (width <= 0 || height <= 0)
-	{
-		return (NULL);
-	}
-
-	grid = malloc(sizeof(int *) * height); /*malloc*/
-
-	if (grid == NULL)
-	{
-		return (NULL);
-	}
-
-	for (i = 0; i < height; i++)
-	{
-		grid[i] = malloc(sizeof(int) * width);
-		if (grid[i] == NULL)
-		{
-			for (i = i - 1; i >= 0; i--)
-			{
-				free(grid[i]);
-			}
-			free(grid);
-			return (NULL);
-		}
-	}
-	for (i = 0; j < width; j++)
-		grid[i][j] = 0;
-	return (grid);
+							if (width <= 0 || height <= 0)
+										return (NULL);
+								matrix = (int **)malloc(height * sizeof(int *));
+									if (matrix == NULL)
+												return (NULL);
+										for (i = 0; i < height; i++)
+												{
+															*(matrix + i) = (int *)malloc(width * sizeof(int));
+																	if (*(matrix + i) == NULL)
+																				{
+																								for (i = 0; i < height; i++)
+																												{
+																																	p = matrix[i];
+																																					free(p);
+																																								}
+																											free(matrix);
+																														return (NULL);
+																																}
+																		}
+											for (l = 0; l < height; l++)
+													{
+																for (j = 0; j < width; j++)
+																			{
+																							matrix[l][j] = 0;
+																									}
+																	}
+												return (matrix);
 }
