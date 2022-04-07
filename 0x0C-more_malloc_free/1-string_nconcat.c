fdfcf4bf4bcd5e229d1e62ca50c61a8ca5314425
yakobsolo <yakisolo2009@gmai.com>
2022-04-07 19:17:12 +0000

    function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,49 +1,115 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
 /**
- * string_nconcat - len of 1st str, len of 2nd str, if n < 2nd, 2nd = n
- * 2nd + 1st = total len, malloc + null byte, loop to insert into temp arr
- * @s1: input one
- * @s2: input two
- * @n: s2's number of bytes
- * Return: 0
+ * _strlen - Counts the lenght of the string
+ * @s: String to analyze
+ *
+ * Return: Lenght of the string
  */
+
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+
+	return (count);
+}
+
+
+/**
+ * _strcpy - Copies the string from a source to a destination
+ * @dest: Where to copy the string
+ * @src: String to copy
+ *
+ * Return: dest's pointer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int count;
+
+	count = 0;
+
+	while (*src != '\0')
+	{
+		*(dest + count) = *src;
+		src++;
+		count++;
+	}
+	*(dest + count) = '\0';
+
+	return (dest);
+}
+
+
+/**
+ * _strncat - Concatenates a string up to the n byte
+ * @dest: String to concatenate
+ * @src: String used to concatenate
+ * @n: Amount of bytes to concatenate
+ *
+ * Return: Destination string (dest)
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int count_dest;
+	int count_src;
+
+	count_dest = 0;
+	count_src = 0;
+
+	while (*(dest + count_dest) != '\0')
+	{
+		count_dest++;
+	}
+
+	while (*(src + count_src) != '\0' && count_src < n)
+	{
+		*(dest + count_dest) = *(src + count_src);
+		count_dest++;
+		count_src++;
+	}
+
+	*(dest + count_dest) = '\0';
+
+	return (dest);
+}
+
+
+/**
+ * string_nconcat - Concatenates n bytes of one string into another string
+ * @s1: Destination string (string to concatenate)
+ * @s2: Source string (string to add)
+ * @n: Amount of bytes from s2 to concatenate to s1
+ * Return: Pointer to the concatenated string or NULL on failure
+ */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *arr;
-	unsigned int i, j, co, co_2;
+	char *out;
+	unsigned int len1, len2;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-	}
-
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-	}
-
-	if (n < j)
-		j = n;
-
-	j += i;
-	arr = malloc(sizeof(char *) * (j + 1));
-
-	if (arr == NULL)
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	if (n >= len2)
+		out = malloc(sizeof(char) * (len1 + len2 + 1));
+	else
+		out = malloc(sizeof(char) * (len1 + n + 1));
+	if (out == NULL)
 		return (NULL);
 
-	for (co = 0; co < i; co++)
-		arr[co] = s1[co];
-	for (co_2 = 0; co < j; co_2++)
-	{
-		arr[co] = s2[co_2];
-		co++;
-	}
-	co++;
-	arr[co] = '\0';
-	return (arr);
+	out = _strcpy(out, s1);
+	out = _strncat(out, s2, n);
+	return (out);
 }
