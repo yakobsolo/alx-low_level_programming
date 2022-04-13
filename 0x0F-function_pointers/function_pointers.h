359c4fc2d3fabd582f82d700349532cc713c69cb
yakobsolo <yakisolo2009@gmai.com>
2022-04-13 13:25:00 +0000

    commit

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,8 @@
-#include <stddef.h>
 #include <stdlib.h>
 #ifndef MAIN
 #define MAIN
+#include <stdlib.h>
+#include <string.h>
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
