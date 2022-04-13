fd165c38eed74cf528aad8f6d2ecc3b68c9174eb
yakobsolo <yakisolo2009@gmai.com>
2022-04-13 12:34:53 +0000

    program that performs simple operations.

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,20 +1,59 @@
+#include "3-calc.h"
+/**
+ * op_add - this function adds two numbers
+ * @a:the first number
+ * @b:the second number.
+* Return: return the result of the add.
+*/
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+/**
+ * op_sub - this function sub two numbers
+ * @a:the first number
+ * @b:the second number.
+* Return: return the result of the sub.
+ */
 int op_sub(int a, int b)
 {
-	return ( a - b);
+	return (a - b);
 }
+/**
+ * op_mul - this function multiply two numbers
+ * @a:the first number
+ * @b:the second number.
+* Return: return the result of the multiply.
+*/
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+/**
+ * op_div - this function divide two numbers
+ * @a:the first number
+ * @b:the second number.
+* Return: return the result of the divide.
+ */
 int op_div(int a, int b)
 {
-	return (a / b);
+	if (b != 0)
+		return (a / b);
+	printf("Error\n");
+	exit(100);
 }
+/**
+ * op_mod - this function calcualte the module
+ * @a:the first number
+ * @b:the second number.
+* Return: return the result of the module.
+*/
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
