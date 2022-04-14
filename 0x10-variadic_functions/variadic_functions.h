07eb8221caa7ed94e1fd65244e48b64c7ab0ac23
yakobsolo <yakisolo2009@gmai.com>
2022-04-14 20:29:09 +0000

    COMMIT

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,13 @@
 #ifndef MAIN
 #define MAIN
+#include <stdarg.h>
+
+typedef struct types
+{
+	char z;
+	void (*f)(va_list);
+} types_t;
+int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
