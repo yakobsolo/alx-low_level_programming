053c3dc99eb0960a2671083c8d481afbc1d08209
yakobsolo <yakisolo2009@gmai.com>
2022-04-14 10:16:41 +0000

    correcting commit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -8,9 +8,9 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	va_list args;
 	unsigned int i;
 	int x;
-	va_list args;
 
 	va_start(args, n);
 		for (i = 0; i < n; i++)
