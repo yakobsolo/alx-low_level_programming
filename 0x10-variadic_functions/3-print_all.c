3fe089b11cb1e99a9e1a607611a47a60ad7d8eb9
yakobsolo <yakisolo2009@gmai.com>
2022-04-14 20:23:58 +0000

    COMMIT

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,100 +1,78 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
 /**
- * integer_print - print integers.
-(* a blank line
-*@args: the list of parameters
-* Description: this function prints integers)?
-(* section header: the header of this function is variadic_functions.h)*
-* Return:no return a void func.
-*/
-void integer_print(va_list args)
+ * print_int - prints an int
+ * @args: the list of args
+ */
+void print_int(va_list args)
 {
 	printf("%d", va_arg(args, int));
 }
 /**
- * char_print - print chars.
-(* a blank line
-*@args: the list of parameters
-* Description: this function prints chars)?
-(* section header: the header of this function is variadic_functions.h)*
-* Return:no return a void func.
+ * print_char - prints a char
+ * @args: the list of args
  */
-void char_print(va_list args)
+void print_char(va_list args)
 {
 	printf("%c", va_arg(args, int));
 }
 /**
- * string_print - print strings
-(* a blank line
-*@args: the list of parameters
-* Description: this function prints strings)?
-(* section header: the header of this function is variadic_functions.h)*
-* Return:no return a void func.
-*/
-
-void string_print(va_list args)
-{
-	char *s;
-
-	s = va_arg(args, char *);
-	if (s == NULL)
-		s = "(nil)";
-	printf("%s", s);
-}
-
-/**
- * float_print - print floats.
-(* a blank line
-*@args: the list of parameters
-* Description: this function prints floats)?
-(* section header: the header of this function is variadic_functions.h)*
-* Return:no return a void func.
+ * print_string - prints a string
+ * @args: the list of args
  */
-void float_print(va_list args)
+void print_string(va_list args)
+{
+	char *z = va_arg(args, char *);
+
+	if (!z)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", z);
+}
+/**
+ * print_float - prints floats
+ * @args: the list of args
+ */
+void print_float(va_list args)
 {
 	printf("%f", va_arg(args, double));
 }
 /**
- * print_all - print anything.
-(* a blank line
-*@format: the paramaters
-* Description: this function prints anything)?
-(* section header: the header of this function is variadic_functions.h)*
-* Return: this function no return
-*/
-
+ * print_all - prints all
+ * @format: formats of arg
+ */
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	int i, j;
-	char *separator;
-	args_t arguments[] = {
-		{"c", char_print},
-		{"i", integer_print},
-		{"f", float_print},
-		{"s", string_print},
-		{NULL, NULL}
+	types_t types[] = {
+	{'c', print_char},
+	{'i', print_int},
+	{'f', print_float},
+	{'s', print_string},
+	{'\0', NULL}
 	};
+	va_list args;
+	char *sep1 = "", *sep2 = ", ";
+	int count1 = 0, count2 = 0;
 
 	va_start(args, format);
-	i = 0;
-	separator = "";
-
-	while (format != NULL && *(format + i) != '\0')
+	while (format !=  NULL && format[count1] != '\0')
 	{
-		j = 0;
-		while (j < 4)
+		count2 = 0;
+		while (types[count2].z != '\0')
 		{
-			if (*(format + i) == *(arguments[j]).format)
+			if (format[count1] == types[count2].z)
 			{
-				printf("%s", separator);
-				arguments[j].function(args);
-				separator = ", ";
-
+				printf("%s", sep1);
+				types[count2].f(args);
+				sep1 = sep2;
 			}
-			j++;
+			count2++;
 		}
-		i++;
+		count1++;
 	}
 	printf("\n");
 	va_end(args);
