9ffab05f920bbfa8a9f39cdec287d3c14c72bf1c
yakobsolo <yakisolo2009@gmai.com>
2022-04-14 20:02:45 +0000

    COMMIT

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,9 +23,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		}
 		else
 		{
-			printf("%p", (void *)x);
+			printf("(nil");
 		}
-		if (separator && i < n - 1)
+		if (separator != NULL && i < n - 1)
 		{
 			printf("%s ", separator);
 		}
