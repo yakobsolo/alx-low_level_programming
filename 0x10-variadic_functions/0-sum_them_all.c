5434759f26ed5ba1bf908ac93076a6db26101b4d
yakobsolo <yakisolo2009@gmai.com>
2022-04-14 10:14:13 +0000

    correcting commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,12 +10,12 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i, x;
 	int sum = 0;
+	 va_list args;
 
 	if (n == 0)
 	{
 		return (0);
 	}
-	va_list args;
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
