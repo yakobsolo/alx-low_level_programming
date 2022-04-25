f8ce597db58d74151236e046536356df5677a2ee
yakobsolo <yakisolo2009@gmai.com>
2022-04-25 08:53:02 +0000

    function that frees

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_listint - function that frees a listint_t
+ * @head: pointer to listint_t struct
+ *
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr = *head;
+	listint_t *new;
+
+	while (ptr)
+	{
+		new = ptr->next;
+		free(ptr);
+		ptr = new;
+	}
+	ptr = NULL;
+	*head = NULL;
+}
