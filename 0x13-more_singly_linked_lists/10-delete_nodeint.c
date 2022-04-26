69ddf3720e4b3ab9fab5d39b9e91c169224012d1
yakob <99880602+yakobsolo@users.noreply.github.com>
2022-04-26 16:20:28 +0300

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -21,7 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
                 {
                         return (-1);
                 }
-                *head = *head->next;
+                *head = ptr2->next;
                free(ptr2);
                 return (1);
         }
